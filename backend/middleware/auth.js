698e8c6dd18c232e33ba29f8edd90af9639e5e19
RGPin <157803400+RGPin@users.noreply.github.com>
2026-06-09 15:59:52 +0800

    set up multer and fix export

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -1,6 +1,10 @@
-export const checkAuth = (req, res, next) => {
+const checkAuth = (req, res, next) => {
   if (!req.isAuthenticated() || !req.user) {
     return res.status(401).json({ message: "User not authenticated" });
   }
   return next();
+};
+
+module.exports = {
+  checkAuth,
 };
